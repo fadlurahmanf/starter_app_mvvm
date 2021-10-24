03c2663cf394f2d7b8c6841389d0f00b7cda90fe
tffaj <taufik.fajari@akarinti.tech>
2021-10-24 11:02:51 +0700

    setup ci github actions 4

--- app/src/main/java/com/fadlurahmanf/starterappmvvm/MainActivity.kt
+++ app/src/main/java/com/fadlurahmanf/starterappmvvm/MainActivity.kt
@@ -3,7 +3,7 @@ package com.fadlurahmanf.starterappmvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// TODO: 24/10/2021 tes new branch 2
+// TODO: 24/10/2021 tes new branch 4
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
