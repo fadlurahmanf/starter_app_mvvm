a993d0d6dce99d60f08df99eeed7aba1a8eb1a84
fadlurahmanf <tffajari@gmail.com>
2023-07-09 13:36:19 +0700

    delete signature

--- app/src/main/java/com/fadlurahmanf/starterappmvvm/feature/encrypt/presentation/CryptoECC.kt
+++ app/src/main/java/com/fadlurahmanf/starterappmvvm/feature/encrypt/presentation/CryptoECC.kt
@@ -10,7 +10,6 @@ import java.security.KeyPairGenerator
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.Security
-import java.security.Signature
 import java.security.spec.ECGenParameterSpec
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
