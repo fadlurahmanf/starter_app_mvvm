2f3d1ee8a605ab46dbda22d08c5b0088d6de5ec8
fadlurahmanf <tffajari@gmail.com>
2023-07-11 20:56:21 +0700

    fix set list

--- app/src/main/java/com/fadlurahmanf/starterappmvvm/feature/download/domain/service/DownloadService.kt
+++ app/src/main/java/com/fadlurahmanf/starterappmvvm/feature/download/domain/service/DownloadService.kt
@@ -15,13 +15,14 @@ import androidx.core.content.ContextCompat
 import com.fadlurahmanf.starterappmvvm.R
 import com.fadlurahmanf.starterappmvvm.core.data.constant.logConsole
 import com.fadlurahmanf.starterappmvvm.core.data.dto.exception.CustomException
+import com.fadlurahmanf.starterappmvvm.feature.download.external.helper.DownloadHelper
 import com.fadlurahmanf.starterappmvvm.feature.download.external.helper.DownloadNotificationHelper
 import com.fadlurahmanf.starterappmvvm.feature.notification.data.constant.NotificationConstant
 import java.io.File
 import kotlin.random.Random
 
 class DownloadService : Service() {
-    private lateinit var downloadManager: DownloadManager
+    private lateinit var downloadHelper: DownloadHelper
     private lateinit var notificationImpl: DownloadNotificationHelper
     private lateinit var handler: Handler
     private var downloadId: Long = 0L
@@ -58,8 +59,7 @@ class DownloadService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        downloadManager =
-            applicationContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadHelper = DownloadHelper(applicationContext)
         notificationImpl = DownloadNotificationHelper(applicationContext)
         handler = Handler(Looper.getMainLooper())
     }
@@ -74,115 +74,49 @@ class DownloadService : Service() {
     }
 
     private fun actionDownload(intent: Intent?) {
-        startForeground(
-            notificationId,
-            notificationImpl.notificationPrepareDownload().build()
-        )
-        val urlDownload = intent?.extras?.getString(DOWNLOAD_URL)
-        val downloadFileName = intent?.extras?.getString(DOWNLOAD_FILE_NAME)
-        if (urlDownload.isNullOrEmpty()) {
+        try {
+            startForeground(
+                notificationId,
+                notificationImpl.notificationPrepareDownload().build()
+            )
+            val urlDownload = intent?.extras?.getString(DOWNLOAD_URL)
+            val downloadFileName = intent?.extras?.getString(DOWNLOAD_FILE_NAME)
+            downloadId = downloadHelper.getDownloadId(
+                urlDownload ?: "",
+                downloadFileName ?: ""
+            )
+            handler.postDelayed(downloadRunnable, 0)
+        } catch (e: Throwable) {
             showError(
                 CustomException(
-                    idRawMessage = R.string.download_url_not_valid_desc
+                    idRawMessage = R.string.download_failed_desc
                 )
             )
+            handler.removeCallbacks(downloadRunnable)
             stopSelf()
-            return
         }
-        val file =
-            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path)
-        val downloadRequest = DownloadManager.Request(Uri.parse(urlDownload))
-        downloadRequest.apply {
-            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-        }
-        val uri =
-            Uri.withAppendedPath(Uri.fromFile(file), "$downloadFileName")
-        downloadRequest.setDestinationUri(uri)
-        downloadId = downloadManager.enqueue(downloadRequest)
-        handler.postDelayed(downloadRunnable, 0)
     }
 
     private var downloadRunnable = object : Runnable {
         override fun run() {
             try {
-                val cursor =
-                    downloadManager.query(DownloadManager.Query().setFilterById(downloadId))
-                if (cursor.moveToFirst()) {
-                    val status: Int = getStatus(cursor)
-                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
-                        handler.removeCallbacks(this)
-                        stopSelf()
-                        return
-                    } else if (status == DownloadManager.STATUS_FAILED) {
-                        showError(
-                            CustomException(
-                                idRawMessage = R.string.download_failed_desc
-                            )
-                        )
-                        handler.removeCallbacks(this)
-                        stopSelf()
-                        return
-                    }
-                    val totalSize: Int = getTotal(cursor)
-                    val progress: Int = getProgress(cursor)
-                    if (totalSize >= 0 && progress >= 0) {
-                        val data = Bundle()
-                        data.apply {
-                            putInt(DownloadNotificationHelper.EXTRA_TOTAL_SIZE, totalSize)
-                            putInt(
-                                DownloadNotificationHelper.EXTRA_CURRENT_PROGRESS_SIZE,
-                                progress
-                            )
-                        }
-                        if (totalSize < progress) {
-                            notificationImpl.showNotification(
-                                title = applicationContext.getString(R.string.download),
-                                body = applicationContext.getString(R.string.downloading)
-                            )
-                        } else {
-                            notificationImpl.showDownload(data)
-                        }
-                    }
+                val status = downloadHelper.updateProgressDownload(downloadId)
+                if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    handler.removeCallbacks(this)
+                    stopSelf()
+                } else {
+                    handler.postDelayed(this, 2000)
                 }
-                handler.postDelayed(this, 2000)
             } catch (e: Throwable) {
                 showError(
                     CustomException(
                         rawMessage = e.message
                     )
                 )
-                stopSelf()
                 handler.removeCallbacks(this)
-                return
+                stopSelf()
             }
         }
-    }
-
-    private fun getStatus(cursor: Cursor): Int {
-        var status: Int = -1
-        if (cursor.getColumnIndex(DownloadManager.COLUMN_STATUS) >= 0) {
-            status = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))
-        }
-        return status
-    }
-
-    private fun getTotal(cursor: Cursor): Int {
-        var totalSize: Int = -1
-        if (cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES) >= 0) {
-            totalSize =
-                cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
-        }
-        return totalSize
-    }
-
-    private fun getProgress(cursor: Cursor): Int {
-        var progress: Int = -1
-        if (cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR) >= 0) {
-            progress =
-                cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
-        }
-        return progress
     }
 
     fun showError(e: CustomException) {
---
+++ app/src/main/java/com/fadlurahmanf/starterappmvvm/feature/download/external/helper/DownloadHelper.kt
@@ -0,0 +1,100 @@
+package com.fadlurahmanf.starterappmvvm.feature.download.external.helper
+
+import android.app.DownloadManager
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import com.fadlurahmanf.starterappmvvm.R
+import com.fadlurahmanf.starterappmvvm.core.data.dto.exception.CustomException
+import java.io.File
+
+class DownloadHelper(
+    private val applicationContext: Context
+) {
+    private var downloadManager: DownloadManager =
+        applicationContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+    private var notificationImpl: DownloadNotificationHelper =
+        DownloadNotificationHelper(applicationContext)
+
+    fun getDownloadId(urlDownload: String, downloadFileName: String): Long {
+        if (urlDownload.isEmpty() || downloadFileName.isEmpty()) {
+            throw CustomException(
+                idRawMessage = R.string.download_url_not_valid_desc
+            )
+        }
+        val file =
+            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path)
+        val downloadRequest = DownloadManager.Request(Uri.parse(urlDownload))
+        downloadRequest.apply {
+            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        }
+        val uri =
+            Uri.withAppendedPath(Uri.fromFile(file), downloadFileName)
+        downloadRequest.setDestinationUri(uri)
+        return downloadManager.enqueue(downloadRequest)
+    }
+
+    fun updateProgressDownload(downloadId: Long): Int {
+        val cursor = downloadManager.query(DownloadManager.Query().setFilterById(downloadId))
+        if (cursor.moveToFirst()) {
+            val status: Int = getStatus(cursor)
+            if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                return DownloadManager.STATUS_SUCCESSFUL
+            } else if (status == DownloadManager.STATUS_FAILED) {
+                throw CustomException(
+                    idRawMessage = R.string.download_failed_desc
+                )
+            }
+            val totalSize: Int = getTotal(cursor)
+            val progress: Int = getProgress(cursor)
+            if (totalSize >= 0 && progress >= 0) {
+                val data = Bundle()
+                data.apply {
+                    putInt(DownloadNotificationHelper.EXTRA_TOTAL_SIZE, totalSize)
+                    putInt(
+                        DownloadNotificationHelper.EXTRA_CURRENT_PROGRESS_SIZE,
+                        progress
+                    )
+                }
+                if (totalSize < progress) {
+                    notificationImpl.showNotification(
+                        title = applicationContext.getString(R.string.download),
+                        body = applicationContext.getString(R.string.downloading)
+                    )
+                } else {
+                    notificationImpl.showDownload(data)
+                }
+            }
+        }
+        return DownloadManager.STATUS_RUNNING
+    }
+
+    private fun getStatus(cursor: Cursor): Int {
+        var status: Int = -1
+        if (cursor.getColumnIndex(DownloadManager.COLUMN_STATUS) >= 0) {
+            status = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))
+        }
+        return status
+    }
+
+    private fun getTotal(cursor: Cursor): Int {
+        var totalSize: Int = -1
+        if (cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES) >= 0) {
+            totalSize =
+                cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+        }
+        return totalSize
+    }
+
+    private fun getProgress(cursor: Cursor): Int {
+        var progress: Int = -1
+        if (cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR) >= 0) {
+            progress =
+                cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+        }
+        return progress
+    }
+}
