69cb0e3b8a3332825ef6cb763c4da0c2b8c7889b
fadlurahmanf <tffajari@gmail.com>
2023-07-13 07:54:12 +0700

    fix

--- app/src/main/java/com/fadlurahmanf/starterappmvvm/core/network/domain/common/BankMasAuthNetwork.kt
+++ app/src/main/java/com/fadlurahmanf/starterappmvvm/core/network/domain/common/BankMasAuthNetwork.kt
@@ -2,16 +2,20 @@ package com.fadlurahmanf.starterappmvvm.core.network.domain.common
 
 import android.content.Context
 import com.fadlurahmanf.starterappmvvm.BuildConfig
+import com.fadlurahmanf.starterappmvvm.core.encrypt.presentation.CryptoAES
 import com.fadlurahmanf.starterappmvvm.unknown.network.authenticator.TokenAuthenticator
+import com.fadlurahmanf.starterappmvvm.unknown.network.interceptor.BankMasAuthInterceptor
 import okhttp3.OkHttpClient
 
-abstract class  AuthAbstractNetwork<T>(
-    context: Context
-): BaseNetwork<T>(context){
-    override fun getBaseUrl(): String = BuildConfig.BASE_URL
+abstract class BankMasAuthNetwork<T>(context: Context, private val cryptoAES: CryptoAES) :
+    BaseNetwork<T>(context) {
+    override fun getBaseUrl(): String {
+        return BuildConfig.BASE_URL
+    }
 
     override fun okHttpClientBuilder(builder: OkHttpClient.Builder): OkHttpClient.Builder {
         return super.okHttpClientBuilder(builder)
+            .addInterceptor(BankMasAuthInterceptor(context, cryptoAES))
             .authenticator(TokenAuthenticator(context))
     }
 }
